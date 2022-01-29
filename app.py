a7d13197e60153cc84c503a8803c736dab5e70d6
anndemo <anndemo61@yandex.ru>
2022-01-29 08:45:01 +0700

    add db

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ storage = MongodbService.get_instance()
 print("0!!!")
 
 
-for row in range(10):
-    hello = {
-        "_id": str(uuid4()),
-        "time": str(int(time.time())),
-        "string": f"Hello, {row} times!"
-    }
-    storage.save_data(hello)
+# for row in range(10):
+#     hello = {
+#         "_id": str(uuid4()),
+#         "time": str(int(time.time())),
+#         "string": f"Hello, {row} times!"
+#     }
+#     storage.save_data(hello)
 
 # num = int(os.environ.get("NUMBER"))
 
